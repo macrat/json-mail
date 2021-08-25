309c32af1bb6d0e8b712362cffca9d0f9c0a7795
MacRat <m@crat.jp>
2021-08-25 13:43:04 +0900

    add: flag parser

--- main.go
+++ main.go
@@ -2,11 +2,65 @@ package main
 
 import (
 	"encoding/json"
-	"os"
+	"flag"
 	"fmt"
+	"io"
+	"os"
 )
 
+var (
+	server   = flag.String("server", "", "SMTP server address")
+	username = flag.String("username", "", "Username for login SMTP server")
+	password = flag.String("password", "", "Password for login SMTP server")
+)
+
+func parseEnvironmentVariable() {
+	if *server == "" {
+		*server = os.Getenv("JSON_MAIL_SERVER")
+	}
+	if *username == "" {
+		*username = os.Getenv("JSON_MAIL_USERNAME")
+	}
+	if *password == "" {
+		*password = os.Getenv("JSON_MAIL_PASSWORD")
+	}
+}
+
+func checkFlagsIfOK(out io.Writer) (ok bool) {
+	var msgs []string
+	if *server == "" {
+		msgs = append(msgs, "--server is required.")
+	}
+	if *username == "" {
+		msgs = append(msgs, "--username is required.")
+	}
+	if *password == "" {
+		msgs = append(msgs, "--password is required.")
+	}
+
+	if msgs != nil {
+		fmt.Fprintln(os.Stderr, "error:")
+		for _, m := range msgs {
+			fmt.Fprintln(os.Stderr, " ", m)
+		}
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "Please see --help to more detail.")
+	}
+
+	return msgs == nil
+}
+
 func main() {
+	flag.Parse()
+	parseEnvironmentVariable()
+	if !checkFlagsIfOK(os.Stderr) {
+		os.Exit(2)
+	}
+
+	fmt.Println("server:", *server)
+	fmt.Println("username:", *username)
+	fmt.Println("password:", *password)
+
 	s := NewMailScanner(os.Stdin)
 
 	for s.Scan() {
